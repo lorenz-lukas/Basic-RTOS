ae363795d238d8611e79f665c92615134c2ea527
lorenz-lukas <lukaslorenzdeandrade@gmail.com>
2020-01-24 05:30:09 -0300

    refatorando

--- Modulo1-RTOS/main.c
+++ Modulo1-RTOS/main.c
@@ -1,10 +1,12 @@
-#include <msp430.h> 
+#include <msp430.h>
 #include <stdint.h>
 
 
 /**
  * main.c
  */
+ __attribute__((naked))
+ __attribute__((interrupt(WDT_VECTOR)))
 
 typedef struct{
     void (*pTask)();          // Entrada atual da tarefa
@@ -16,6 +18,24 @@ task_t tasks[10];
 uint16_t taskCounter = 0;
 uint16_t taskNumber = 0;
 void * schedSP = (void*)0x02500;
+
+void registerTask();
+void BlinkLED_RED();
+void BlinkLED_GREEN();
+void startBRTOS();
+void WDT_ISR();
+
+int main(void)
+{
+    registerTask(BlinkLED_RED);
+    registerTask(BlinkLED_GREEN);
+
+    startBRTOS();
+
+    while(1);
+
+    return 0;
+}
 
 
 void registerTask(void * pTask){
@@ -29,13 +49,6 @@ void registerTask(void * pTask){
     *(tasks[taskCounter].pStack) = ((uint32_t)pTask >> 4) & 0xF000;
     *(tasks[taskCounter].pStack) |= GIE;                         // GIE = SR
 
-
-    // tasks[taskCounter].pStack--;
-    //*(tasks[taskCounter].pStack) |= 0x008;
-    //*(tasks[taskCounter].pStack) = (&pTask >> 4) & 0xF000|0X008; //0x008 = SR value
-
-    //tasks[taskCounter].pStack--;
-
     for(int a = 0; a < 24; a ++){                          // Preenche o resto da pilha com '0'
         tasks[taskCounter].pStack--;
         *(tasks[taskCounter].pStack) = 0;
@@ -45,84 +58,48 @@ void registerTask(void * pTask){
 }
 
 void BlinkLED_RED(){
-
-
     P1DIR |= BIT0;
 
     while(1){
         volatile uint16_t i = 50000;
         while(i--);
-
         P1OUT ^= BIT0;
     }
 }
 
 void BlinkLED_GREEN(){
-
     P4DIR |= BIT7;
-
     while(1){
         volatile uint16_t i = 25000;
         while(i--);
-
         P4OUT ^= BIT7;
     }
 }
 
 void startBRTOS(){
-
     WDTCTL = WDTSSEL__ACLK |    // ACLK
             WDTTMSEL |          // Intervalo
             WDTIS_5 |           // Time interval select
             WDTPW;              // Password;
 
-
     SFRIE1 |= WDTIE;           // Interrupção watchdog_timer
     __enable_interrupt();
-
     tasks[0].pStack += 26;
 
     asm("movx.a %0,SP" :: "m" (tasks[0].pStack));
-
     asm("pushx.a %0" :: "m" (tasks[0].pTask));
-
 }
-
-int main(void)
-{
-
-    registerTask(BlinkLED_RED);
-    registerTask(BlinkLED_GREEN);
-
-    startBRTOS();
-
-    while(1);
-
-
-    return 0;
-}
-
-__attribute__((naked))
-__attribute__((interrupt(WDT_VECTOR)))
 
 void WDT_ISR(){
     //Executa uma tarefa e atualiza pTask
-
     asm("pushm.a #12,R15" );    //Salva contexto
-
     asm("movx.a SP, %0" : "=m" (tasks[taskNumber].pStack));
-
     asm("movx.a %0,SP" :: "m" (schedSP));
 
     taskNumber = (taskNumber + 1) % taskCounter;
 
     asm("movx.a SP,%0" : "=m" (schedSP));
-
     asm("movx.a %0,SP" :: "m" (tasks[taskNumber].pStack));
-
     asm("popm.a #12,R15");
     asm("RETI");
-
-
 }
-
